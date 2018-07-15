9b11739610cadf0de3c890b309b852c137cca13e
Alexiy <alexiy.ov@gmail.com>
2018-07-15 10:31:47 +0300

    Method test

---
+++ src/test/java/MethodsTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.knowbase.tools.Methods;
+
+import java.util.HashMap;
+
+/**
+ * Created on 7/15/18 by alexiy.
+ */
+
+public class MethodsTest {
+
+    @Test
+    public void testMapCreation()
+    {
+        HashMap hashMap=Methods.createMapFrom("Java:*.java","Groovy:*.groovy");
+        Assert.assertTrue(hashMap.size()>0);
+        System.out.println(hashMap);
+    }
+}
