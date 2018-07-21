c3b02b3bce3a0819b306d5275005a87c3bfed95a
Alexiy <alexiy.ov@gmail.com>
2018-07-21 22:06:46 +0300

    Default V- and HBox implementations

---
+++ src/main/java/org/knowbase/Hbox2.java
@@ -0,0 +1,20 @@
+package org.knowbase;
+
+import javafx.scene.Node;
+import javafx.scene.layout.HBox;
+
+/**
+ * Created on 7/21/18 by alexiy.
+ */
+public class Hbox2 extends HBox {
+
+    public Hbox2() {
+        super(6);
+    }
+
+    public Hbox2(Node... children) {
+        super(6,children);
+    }
+
+
+}
---
+++ src/main/java/org/knowbase/Vbox2.java
@@ -0,0 +1,17 @@
+package org.knowbase;
+
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+
+/**
+ * Created on 7/21/18 by alexiy.
+ */
+public class Vbox2 extends VBox {
+    public Vbox2() {
+        super(6);
+    }
+
+    public Vbox2(Node... children) {
+        super(6,children);
+    }
+}
