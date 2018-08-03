651b43f921df2dacf59e9b3fbef1b3eee8df96d3
Alexiy <alexiy.ov@gmail.com>
2018-08-03 07:14:35 +0300

    Added a type parameter which defines dialog's content node

--- src/main/java/org/knowbase/Dialog2.java
+++ src/main/java/org/knowbase/Dialog2.java
@@ -7,13 +7,25 @@ import javafx.stage.Modality;
 
 /**
  * Created on 7/13/18 by alexiy.
+ * @param <T> return value class
+ * @param <C> container class
  */
-public class Dialog2<T> extends Dialog<T> {
-    public Dialog2(String title, Node container, Modality modality, ButtonType... buttonTypes) {
+@SuppressWarnings("unchecked")
+public class Dialog2<T,C extends Node> extends Dialog<T> {
+    /**
+     *
+     * @param container dialog's main container
+     */
+    public Dialog2(String title, C container, Modality modality, ButtonType... buttonTypes) {
         setTitle(title);
         getDialogPane().setContent(container);
         getDialogPane().getButtonTypes().addAll(buttonTypes);
         if(modality!=null)
             initModality(modality);
+    }
+
+    public C getContainer()
+    {
+        return (C) getDialogPane().getContent();
     }
 }
